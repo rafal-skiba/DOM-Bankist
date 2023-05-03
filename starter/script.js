e3135bf725350c8ae9de19125971870c24a28c5f
Rafał Skiba <rr.skiba92@gmail.com>
2023-05-03 20:58:17 +0200

    scrolly smooth to first section added

--- starter/script.js
+++ starter/script.js
@@ -28,3 +28,14 @@ document.addEventListener('keydown', function (e) {
     closeModal();
   }
 });
+
+const btnScroll = document.querySelector(".btn--scroll-to");
+
+const section1 = document.querySelector('#section--1');
+
+btnScroll.addEventListener("click", (e) => {
+ 
+ section1.scrollIntoView({behavior: "smooth"});
+
+})
+
