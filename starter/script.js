6309a0fd7a8abf15cc1a0ede1d3e48240b61d56e
Rafał Skiba <rr.skiba92@gmail.com>
2023-05-18 20:58:18 +0200

    modified sticky nav function

--- starter/script.js
+++ starter/script.js
@@ -90,17 +90,38 @@ document.querySelector(`.operations__content--${clicked.dataset.tab}`).classList
 })
 
 // Nav Sticky position
-const inintialCoords = section1.getBoundingClientRect();
 
-window.addEventListener('scroll', function(e) {
- if (this.window.scrollY > inintialCoords.top) {
+// const obsCallback = function (entries, observer) {
+//   entries.forEach(entry => {console.log(entry)})
+// };
+
+// const obsOptions = {};
+
+// const observer = new IntersectionObserver(obsCallback, {
+//   root: null,
+//   threshold: 0.5,
+// });
+
+// observer.observe(section1);
+
+const header = document.querySelector('.header');
+const navHeight = nav.getBoundingClientRect().height;
+
+const stickyNav = function (entries) {
+const [entry] = entries;
+
+if (!entry.isIntersecting) {
   nav.classList.add('sticky');
- } else {
-  nav.classList.remove('sticky');
- }
+} else nav.classList.remove('sticky')
+}
 
+const headerObserver = new IntersectionObserver( stickyNav, {
+root: null,
+threshold: 0,
+rootMargin: `-${navHeight}px`
+});
 
-})
+headerObserver.observe(header);
 
 
 
