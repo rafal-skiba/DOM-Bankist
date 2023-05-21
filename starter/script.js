647ef446c0768aef2271df45a2abf425601400ac
Rafał Skiba <rr.skiba92@gmail.com>
2023-05-21 22:32:40 +0200

    smooth image loading

--- starter/script.js
+++ starter/script.js
@@ -134,6 +134,34 @@ allSection.forEach(function(section) {
   sectionObserver.observe(section);
   section.classList.add('section--hidden');
 
-} )
+} ) 
 
+
+// Lazy loading images
+
+const imgTargets = document.querySelectorAll('img[data-src]');
+
+const lazyImgFn = function (entries, observer) {
+ const [entry] = entries;
+
+ if (!entry.isIntersecting) return;
+
+ // replace src with data-src
+ entry.target.src = entry.target.dataset.src;
+ 
+
+ entry.target.addEventListener('load', function () {
+  entry.target.classList.remove('lazy-img');
+ })
+
+ observer.unobserve(entry.target);
+}
+
+const imgObserver = new IntersectionObserver( lazyImgFn, {
+  root: null,
+  threshold: 0,
+  rootMargin: '200px'
+})
+
+imgTargets.forEach(img => imgObserver.observe(img));
 
